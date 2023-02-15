a5b66d79b133e9274ff82b65536aa59653da03ff
Phodal Huang <h@phodal.com>
2023-02-15 20:36:03 +0800

    feat: try to write with throw

--- src/main/kotlin/com/phodal/chapi/arrow/Main.kt
+++ src/main/kotlin/com/phodal/chapi/arrow/Main.kt
@@ -2,16 +2,55 @@ package com.phodal.chapi.arrow
 
 import org.apache.arrow.memory.RootAllocator
 import org.apache.arrow.vector.IntVector
+import org.apache.arrow.vector.VarCharVector
+import org.apache.arrow.vector.VectorSchemaRoot
+import org.apache.arrow.vector.ipc.ArrowFileWriter
+import org.apache.arrow.vector.types.pojo.ArrowType
+import org.apache.arrow.vector.types.pojo.Field
+import org.apache.arrow.vector.types.pojo.FieldType
+import org.apache.arrow.vector.types.pojo.Schema
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.Arrays.asList
+
 
 fun main(args: Array<String>) {
+
     RootAllocator().use { allocator ->
-        IntVector("fixed-size-primitive-layout", allocator).use { intVector ->
-            intVector.allocateNew(3)
-            intVector[0] = 1
-            intVector.setNull(1)
-            intVector[2] = 2
-            intVector.valueCount = 3
-            println("Vector created in memory: $intVector")
+        val name = Field("name", FieldType.nullable(ArrowType.Utf8()), null)
+        val age = Field("age", FieldType.nullable(ArrowType.Int(32, true)), null)
+
+        val schemaPerson = Schema(listOf(name, age))
+
+        VectorSchemaRoot.create(schemaPerson, allocator).use { vectorSchemaRoot ->
+            val nameVector = vectorSchemaRoot.getVector("name") as VarCharVector
+            nameVector.allocateNew(3)
+            nameVector[0] = "David".toByteArray()
+            nameVector[1] = "Gladis".toByteArray()
+            nameVector[2] = "Juan".toByteArray()
+            val ageVector =
+                vectorSchemaRoot.getVector("age") as IntVector
+            ageVector.allocateNew(3)
+            ageVector[0] = 10
+            ageVector[1] = 20
+            ageVector[2] = 30
+            vectorSchemaRoot.rowCount = 3
+            val file = File("randon_access_to_file.arrow")
+            try {
+                FileOutputStream(file).use { fileOutputStream ->
+                    ArrowFileWriter(vectorSchemaRoot, null, fileOutputStream.channel).use { writer ->
+                        writer.start()
+                        writer.writeBatch()
+                        writer.end()
+                        println("Record batches written: " + writer.recordBlocks.size + ". Number of rows written: " + vectorSchemaRoot.rowCount)
+                    }
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
         }
     }
+
+
 }
