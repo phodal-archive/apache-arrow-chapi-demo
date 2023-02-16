47ee5f116d801c9c3f2cf1569f1df594e0dbdae4
Phodal Huang <h@phodal.com>
2023-02-16 16:24:10 +0800

    docs: add sample for testdatA

--- src/main/kotlin/com/phodal/chapi/arrow/Main.kt
+++ src/main/kotlin/com/phodal/chapi/arrow/Main.kt
@@ -5,8 +5,8 @@
 package com.phodal.chapi.arrow
 
 import chapi.domain.core.CodeDataStruct
-import chapi.domain.core.CodeExport
-import chapi.domain.core.CodeField
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.api.cast
 import org.jetbrains.kotlinx.dataframe.api.print
@@ -18,42 +18,15 @@ import java.io.File
 private const val FILE_NAME = "0_codes.arrow"
 
 fun main(args: Array<String>) {
-    val codeDataStructs = listOf(
-        CodeDataStruct(
-            NodeName = "MainCli",
-            Package = "com.phodal.chapi",
-            Fields = arrayListOf(CodeField("name", "string", "test")),
-            MultipleExtend = arrayListOf("Sample"),
-            Extend = "Client",
-            Exports = arrayListOf(CodeExport("test", "test"))
-        )
-    )
+    val dataString = File("0_codes.json").readText()
+    val codeDataStructs: List<CodeDataStruct> = Json.decodeFromString(dataString)
 
     // maxDepth = 1
-    codeDataStructs.toDataFrame(maxDepth = 2).writeArrowFeather(File(FILE_NAME))
+    codeDataStructs.toDataFrame()
+        .writeArrowFeather(File(FILE_NAME))
     val dataFrame = DataFrame
         .readArrowFeather(File(FILE_NAME))
 
     dataFrame
         .print(10)
-}
-
-private fun sampleWithArray() {
-    data class Name(val firstName: String, val lastName: String)
-    data class Score(val subject: String, val value: Int)
-    data class Student(val name: Name, val age: Int, val scores: Array<Score>)
-
-    val students = listOf(
-        Student(Name("Alice", "Cooper"), 15, arrayOf(Score("math", 4), Score("biology", 3))),
-        Student(Name("Bob", "Marley"), 20, arrayOf(Score("music", 5)))
-    )
-
-    val df = students.toDataFrame(maxDepth = 1)
-    df.writeArrowFeather(File(FILE_NAME))
-
-    val dataFrame = DataFrame
-        .readArrowFeather(FILE_NAME)
-        .cast<Student>()
-
-    dataFrame.print(10)
 }
