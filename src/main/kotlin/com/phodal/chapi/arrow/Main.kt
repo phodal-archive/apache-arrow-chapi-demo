1fe9fec53021faa78abe4d4004c0d9cc5bd9f52a
Phodal Huang <h@phodal.com>
2023-02-16 16:36:34 +0800

    docs: add sample code for testdata

--- src/main/kotlin/com/phodal/chapi/arrow/Main.kt
+++ src/main/kotlin/com/phodal/chapi/arrow/Main.kt
@@ -11,6 +11,7 @@ import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.api.cast
 import org.jetbrains.kotlinx.dataframe.api.print
 import org.jetbrains.kotlinx.dataframe.api.toDataFrame
+import org.jetbrains.kotlinx.dataframe.io.read
 import org.jetbrains.kotlinx.dataframe.io.readArrowFeather
 import org.jetbrains.kotlinx.dataframe.io.writeArrowFeather
 import java.io.File
@@ -18,15 +19,13 @@ import java.io.File
 private const val FILE_NAME = "0_codes.arrow"
 
 fun main(args: Array<String>) {
-    val dataString = File("0_codes.json").readText()
-    val codeDataStructs: List<CodeDataStruct> = Json.decodeFromString(dataString)
+    val dataFrame = DataFrame.read("data${File.separator}0_codes.json")
 
     // maxDepth = 1
-    codeDataStructs.toDataFrame()
-        .writeArrowFeather(File(FILE_NAME))
-    val dataFrame = DataFrame
+    dataFrame.writeArrowFeather(File(FILE_NAME))
+    val dataFrame2 = DataFrame
         .readArrowFeather(File(FILE_NAME))
 
-    dataFrame
+    dataFrame2
         .print(10)
 }
