138b650602268934c9edf083bce8c56323c792de
Phodal Huang <h@phodal.com>
2023-02-17 12:41:51 +0800

    docs: simplify dta

--- src/main/kotlin/com/phodal/chapi/arrow/Main.kt
+++ src/main/kotlin/com/phodal/chapi/arrow/Main.kt
@@ -1,12 +1,19 @@
 package com.phodal.chapi.arrow
 
 import chapi.domain.core.CodeDataStruct
+import org.apache.arrow.memory.RootAllocator
+import org.apache.arrow.vector.VarCharVector
+import org.apache.arrow.vector.VectorSchemaRoot
+import org.apache.arrow.vector.ipc.ArrowFileWriter
+import org.apache.arrow.vector.types.pojo.ArrowType
 import org.apache.arrow.vector.types.pojo.Field
 import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.api.cast
 import org.jetbrains.kotlinx.dataframe.io.ignoreMismatchMessage
 import org.jetbrains.kotlinx.dataframe.io.read
 import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 
 fun main(args: Array<String>) {
@@ -22,12 +29,23 @@ fun main(args: Array<String>) {
     val toArrowSchema = org.apache.arrow.vector.types.pojo.Schema(fields)
     File("schema.json").writeText(toArrowSchema.toJson())
 
-//    val schema = Schema(List<Field>().k().apply {
-//        add(Field.nullable("Source", ArrowType.Utf8()))
-//        add(Field.nullable("AsName", ArrowType.Utf8()))
-//        add(Field.nullable("UsageName", ArrowType.List(ArrowType.Utf8())))
-//        add(Field.nullable("Scope", ArrowType.Utf8()))
-//    }, null)
-
+    RootAllocator().use { allocator ->
+        VectorSchemaRoot.create(toArrowSchema, allocator).use { vectorSchemaRoot ->
+            val file = File("randon_access_to_file.arrow")
+            try {
+                FileOutputStream(file).use { fileOutputStream ->
+                    ArrowFileWriter(vectorSchemaRoot, null, fileOutputStream.channel)
+                        .use { writer ->
+                            writer.start()
+                            writer.writeBatch()
+                            writer.end()
+                            println("Record batches written: " + writer.recordBlocks.size + ". Number of rows written: " + vectorSchemaRoot.rowCount)
+                        }
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
 }
 
