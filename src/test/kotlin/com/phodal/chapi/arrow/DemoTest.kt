1d0fedf01fe7a06744646f6b77cdc5cf3691429f
Phodal Huang <h@phodal.com>
2023-02-17 13:16:53 +0800

    docs: add more samples

--- src/test/kotlin/com/phodal/chapi/arrow/DemoTest.kt
+++ src/test/kotlin/com/phodal/chapi/arrow/DemoTest.kt
@@ -1,15 +1,26 @@
 package com.phodal.chapi.arrow
 
 import com.google.flatbuffers.FlatBufferBuilder
+import kotlinx.serialization.descriptors.PrimitiveKind.*
 import org.apache.arrow.memory.RootAllocator
+import org.apache.arrow.util.Collections2
 import org.apache.arrow.vector.complex.FixedSizeListVector
 import org.apache.arrow.vector.complex.ListVector
+import org.apache.arrow.vector.types.FloatingPointPrecision.DOUBLE
+import org.apache.arrow.vector.types.FloatingPointPrecision.SINGLE
+import org.apache.arrow.vector.types.TimeUnit
+import org.apache.arrow.vector.types.Types.MinorType
+import org.apache.arrow.vector.types.UnionMode
 import org.apache.arrow.vector.types.pojo.ArrowType
+import org.apache.arrow.vector.types.pojo.ArrowType.FloatingPoint
+import org.apache.arrow.vector.types.pojo.ArrowType.Struct
+import org.apache.arrow.vector.types.pojo.ArrowType.Timestamp
+import org.apache.arrow.vector.types.pojo.ArrowType.Union
+import org.apache.arrow.vector.types.pojo.ArrowType.Utf8
+import org.apache.arrow.vector.types.pojo.Field
 import org.apache.arrow.vector.types.pojo.FieldType
-import org.apache.arrow.vector.types.pojo.Field;
 import org.apache.arrow.vector.types.pojo.Schema
 import org.junit.jupiter.api.Test
-
 
 class DemoTest {
     @Test
@@ -37,6 +48,51 @@ class DemoTest {
         val initialSchema = Schema(parent)
         val jsonSchema: String = initialSchema.toJson()
 
+        println(jsonSchema)
+    }
+
+    @Test
+    fun `json schema 2`() {
+        val children: MutableList<Field> = ArrayList()
+        children.add(Field("child1", FieldType.nullable(Utf8.INSTANCE), null))
+        children.add(Field("child2", FieldType.nullable(FloatingPoint(SINGLE)), emptyList<Field>()))
+        children.add(
+            Field(
+                "child3", FieldType.nullable(Struct()), Collections2.asImmutableList<Field>(
+                    Field("child3.1", FieldType.nullable(Utf8.INSTANCE), null),
+                    Field("child3.2", FieldType.nullable(FloatingPoint(DOUBLE)), emptyList<Field>())
+                )
+            )
+        )
+        children.add(
+            Field(
+                "child4", FieldType.nullable(ArrowType.List()), Collections2.asImmutableList<Field>(
+                    Field("child4.1", FieldType.nullable(Utf8.INSTANCE), null)
+                )
+            )
+        )
+        children.add(
+            Field(
+                "child5", FieldType.nullable(
+                    Union(UnionMode.Sparse, intArrayOf(MinorType.TIMESTAMPMILLI.ordinal, MinorType.FLOAT8.ordinal))
+                ),
+                Collections2.asImmutableList<Field>(
+                    Field("child5.1", FieldType.nullable(Timestamp(TimeUnit.MILLISECOND, null)), null),
+                    Field("child5.2", FieldType.nullable(FloatingPoint(DOUBLE)), emptyList<Field>()),
+                    Field("child5.3", FieldType.nullable(Timestamp(TimeUnit.MILLISECOND, "UTC")), null)
+                )
+            )
+        )
+
+        run(Schema(children))
+    }
+
+    private fun run(initialSchema: Schema) {
+        val builder = FlatBufferBuilder()
+        builder.finish(initialSchema.getSchema(builder))
+        val flatBufSchema = org.apache.arrow.flatbuf.Schema.getRootAsSchema(builder.dataBuffer())
+        val finalSchema = Schema.convertSchema(flatBufSchema)
+        val jsonSchema: String = finalSchema.toJson()
         println(jsonSchema)
     }
 }
